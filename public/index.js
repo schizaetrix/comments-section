2468d1b0f17812ad01e6a0f2758a22a3935c4696
Chiza Barber <chiza.alba@gmail.com>
2018-11-10 09:05:52 -0600

    Added NOTEs and TODOs

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 // Initialize Firebase
+
 const config = {
     apiKey: "AIzaSyDD_EmUyHKXNCBPwb6HVjF74XI5GdhrcBE",
     authDomain: "comments-section-2cf36.firebaseapp.com",
@@ -9,13 +10,48 @@ const config = {
 };
 firebase.initializeApp(config);
 
-// Define Realtime Database references & synchronization
+
+//--------------------------------------------------------------
+
+// TODO:    Write function(s) that takes user input from 
+//          the comment form (id='comment-form') and writes
+//          it to the appropriate child nodes in my DB
+// TODO:    Modify the below code so that each object group
+//          (ie. username, email and comment child nodes)
+//          becomes a list item that gets added to the
+//          comment div (id='comment-area')
+
+//--------------------------------------------------------------
+
+// NOTE:    This section prints the data from the 'users' 
+//          object in my Firebase DB to the 'comment-area' 
+//          div in index.html
 const commentArea = document.getElementById('comment-area')
 const dbRefComment = firebase.database().ref().child('users')
 dbRefComment.on('value', snap => {
     commentArea.innerText = JSON.stringify(snap.val(), null, 3)
-})
+})  
+// TODO:        Modify the above code to parse through the
+//              stringified snap value (instead of setting
+//              the innerText of commentArea)
 
+// TODO:        Write a function that takes the object
+//              returned by the parse and creates a template
+//              literal div that formats the comment.
+//              Something like: `
+//              <div>
+//                  <div>${users.username}</div>
+//                  <div>${users.email}</div>
+//                  <div>${users.comment}</div>
+//              </div> `
+
+// TODO:        Modify the below list code so that the above
+//              template literal is added as the innerHTML
+//              of each li tag that is appended to the ul
+
+
+// NOTE:    This section is an example of how to append 'li'
+//          child tags to the 'list' ul tag in index.html
 const ulList = document.getElementById('list')
 const dbRefList = dbRefComment.child('list')
 dbRefList.on('child_added', snap => {
@@ -24,16 +60,13 @@ dbRefList.on('child_added', snap => {
     li.id = snap.key
     ulList.appendChild(li)
 })
+// NOTE:    This section allows you to change list items
 dbRefList.on('child_changed', snap => {
     const liChanged = document.getElementById(snap.key)
     liChanged.innerText = snap.val()
 })
+// NOTE:    This section allows you to remove list items
 dbRefList.on('child_removed', snap => {
     const liRemoved = document.getElementById(snap.key)
     liRemoved.remove()
 })
-
-
-// const username = document.getElementById('username-text')
-// const email = document.getElementById('email-text')
-// const comment = document.getElementById('comment-text')
